e9f57948a5d2426043540d7f0b9815f2d38a184e
oneMelon <1040835678@qq.com>
2020-09-28 14:50:29 +0800

    Initial Commit

---
+++ GitVersionControl/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  GitVersionControl
+//
+//  Created by oneMelon on 2020/9/28.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
